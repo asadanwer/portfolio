c9ff120608bde1b621049563f8d969f10edd3318
Asad Anwer <asad1503b@gmail.com>
2021-01-18 14:22:52 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import Body from './components/Body';
-import RequestForm from './components/RequestForm';
+import React, { useState }  from "react";
+import Body from './components/Body'
 
 function App() {
+  // const [info, setinfo] = useState(data)
+
   return (
     <div>
     <Body/>
+        {/* //here if data is null then does on map to info  */}
+
+        {/* {info && info.map(data=><Learn key={data.id} data ={ data}/>)} */}
+      {/* //if is not empty */}
+      {/* {info ? info.map(data=><Learn key={data.id} data ={ data}/>) :<div>Mo Data is shown</div>} */}
+      {/* {info?.map(data=><Learn key={data.id} data ={ data}/>)} */}
+      {/* //ternay operator  */}
+     
+    
     </div>
   );
 }
